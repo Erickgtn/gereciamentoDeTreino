354fc80f0c7e175de49ca91db9f038b45f26f2df
Eric Sampaio <erick.gtn.xp@gmail.com>
2022-09-07 11:02:46 -0300

    tirando comits

--- meu-app/src/Treino.js
+++ meu-app/src/Treino.js
@@ -69,7 +69,7 @@ function Treino(){
                     name="name"
                     placeholder="Nome"
                     value={name}
-                    // onChange={(e)=> setName(e.target.value)}
+                    onChange={(e)=> setName(e.target.value)}
                 />
             </Col>
             <Col>
@@ -80,7 +80,7 @@ function Treino(){
                     name="sobreNome"
                     placeholder="Sobrenome" 
                     value={sobrenome} 
-                    // onChange={(e)=> setSobrenome(e.target.value)}
+                    onChange={(e)=> setSobrenome(e.target.value)}
                 />
             </Col>
             <Col>
